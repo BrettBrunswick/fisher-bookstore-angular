87bf1ed63c4c3ec14fa8fe146434f713d991db3c
Brett Brunswick <BrettBrunswick@bretts-mbp-2.home>
2018-02-19 22:38:04 -0500

    First commit.

---
+++ src/app/books/mock-books-service.ts
@@ -0,0 +1,6 @@
+import { Book } from './book';
+
+export const BOOKS: Book[] = [
+    {id: 1, title: 'My First Book', author: 'Brett Brunswick'},
+    {id: 2, title: 'Culture II', author: 'Migos'}
+]
